9865dc9a6b5ee19aec2e0afc2ca4b8ce7ba99705
Justin Dampal <justindampal@gmail.com>
2026-03-23 14:35:02 +0800

    feat: add background script with default toggle initialization

--- background/background.js
+++ background/background.js
@@ -1,1 +1,21 @@
-// Background script placeholder
+(function () {
+  "use strict";
+
+  const DEFAULT_SETTINGS = {
+    hideHomepage: true,
+    hideSidebar: true,
+    hideEndScreen: true,
+    hideComments: true,
+    hideShorts: true,
+    hideAds: true,
+    hideTrending: true,
+    hideNotificationBadge: true,
+    hideLiveChat: true,
+  };
+
+  browser.runtime.onInstalled.addListener((details) => {
+    if (details.reason === "install") {
+      browser.storage.local.set(DEFAULT_SETTINGS);
+    }
+  });
+})();
